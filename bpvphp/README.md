61b2402d41030662a4740528c5cd0675fcdfdb44
Unknown <dhrc1997@gmail.com>
2018-11-08 17:48:28 -0200

    bpvPHP

---
+++ bpvphp/README.md
@@ -0,0 +1,5 @@
+Projeto TIS 3 
+
+Basquete pra Vida
+
+PUC Minas - Engenharia de Software
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import thunk from 'redux-thunk';
-import {createStore,compose,applyMiddleware} from 'redux';
-import reducers from '../reducers/';
-
-export default createStore(reducers,compose(
-    applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-));
